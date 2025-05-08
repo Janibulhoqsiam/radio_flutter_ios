a307ef8d7812b197da37638972da9626fdb34e62
Janibulhoqsiam <ononna.jahan.100@gmail.com>
2025-05-09 02:46:19 +0600

    ads integration

--- lib/ad_helper.dart
+++ lib/ad_helper.dart
@@ -3,9 +3,9 @@ class AdHelper {
   static String get bannerAdUnitId{
 
     if(Platform.isAndroid){
-      return 'ca-app-pub-3940256099942544/9214589741';
+      return 'ca-app-pub-4002965790017475/1028444931';
     }else if(Platform.isIOS){
-      return 'ca-app-pub-3940256099942544/9214589741';
+      return 'ca-app-pub-4002965790017475/3234730474';
     }else{
       throw UnsupportedError("UnSuppoerted platform");
     }
@@ -15,9 +15,9 @@ class AdHelper {
   static String get interstitialAd{
 
     if(Platform.isAndroid){
-      return 'ca-app-pub-3940256099942544/1033173712';
+      return 'ca-app-pub-4002965790017475/7676005533';
     }else if(Platform.isIOS){
-      return 'ca-app-pub-3940256099942544/1033173712';
+      return 'ca-app-pub-4002965790017475/1930748475';
     }else{
       throw UnsupportedError("UnSuppoerted platform");
     }
